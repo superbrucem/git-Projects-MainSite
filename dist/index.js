4e86133348fc4f4c59de899de9207a8c86ea1db9
superbrucem <superbrucem@gmail.com>
2025-03-30 19:55:36 -0400

    fixed page not found

--- dist/index.js
+++ dist/index.js
@@ -104,7 +104,7 @@ var MemStorage = class {
   }
   async initDemoProjects() {
     const project1 = await this.createProject({
-      title: "E-commerce Dashboard",
+      title: "This Website",
       description: "A modern dashboard for e-commerce analytics with real-time data visualization.",
       repoUrl: "https://github.com/brucemaber/ecommerce-dashboard",
       imageUrl: "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=800&q=80",
